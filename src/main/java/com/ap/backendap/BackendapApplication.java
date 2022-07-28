bfaf991ea6061f5be604a34930f4b5ddfed84798
xMercurialx <100611284+xMercurialx@users.noreply.github.com>
2022-07-28 13:17:46 -0300

    Initial commit

---
+++ src/main/java/com/ap/backendap/BackendapApplication.java
@@ -0,0 +1,13 @@
+package com.ap.backendap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendapApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ap/backendap/BackendapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ap.backendap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
