17eccb88a08139d6f7a2cfa0a48f745384afe8d6
xMercurialx <100611284+xMercurialx@users.noreply.github.com>
2022-07-29 19:43:21 -0300

    cors

--- src/main/java/com/ap/backendap/BackendapApplication.java
+++ src/main/java/com/ap/backendap/BackendapApplication.java
@@ -6,6 +6,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.Arrays;
 
@@ -15,7 +17,18 @@ public class BackendapApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BackendapApplication.class, args);
 	}
+
+
+
 	@Bean
+	public WebMvcConfigurer corsConfigurer(){return new WebMvcConfigurer() {
+		@Override
+		public void addCorsMappings(CorsRegistry registry){
+			registry.addMapping("/**").allowedOrigins("https://argentinaback.herokuapp.com");
+		}
+	};}
+
+		 @Bean
 	public CorsFilter corsFilter() {
 		CorsConfiguration corsConfiguration = new CorsConfiguration();
 		corsConfiguration.setAllowCredentials(true);
